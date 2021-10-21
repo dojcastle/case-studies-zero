53a2271418bf7916f86e9c1299b10ce62bee74d0
EhrieDaveLimpag <ehrie.limpag@energyweb.org>
2021-10-21 16:34:40 +0800

    content-updates

--- src/scripts/component/credit.js
+++
@@ -1,6 +0,0 @@
-export default function Credit() {
-  console.log(
-    '%cDeveloped by ― Reksa Andhika // http://reksaandhika.com',
-    'background-color: black ; color: white ; font-weight: bold ;'
-  );
-}
